6acc47244cf3e44175bc4c9a96f2e873a0f2de77
Joseph Serrato <joseph.serrato@gmail.com>
2019-03-17 16:41:27 -0700

    added notes of what i did in javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,24 @@
 
 //array of letters. 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var userGuesses = [];
-var compGuess = letters[Math.floor(Math.random() * letters.length)];
+var wins = 0; //wins counter variable.
+var losses = 0; //losses counter variable.
+var guessesLeft = 9; //10 total guesses left counter variable.
+var userGuesses = []; //guesses made by user. 
+var compGuess = letters[Math.floor(Math.random() * letters.length)]; //letters array random generator.
 
 //start function with a key input for letter buttons pushed to appear.
 console.log(compGuess + " is the letter ")
 
-document.onkeyup = function (event) { //triggering the function. 
-  var userGuess = event.key;
+document.onkeyup = function (event) { //triggering the function for every letter pushed. 
+  var userGuess = event.key; //
   userGuesses.push(userGuess);
 
   console.log(userGuess + " is user guess");
   console.log("user guesses so far is " + userGuesses.length, "entries");
 
-
   //logic- if userGuess is the same as the compGuess its counted as a win.
-  if (userGuess == compGuess) { //someone wins and we need to reset the board.
+  if (userGuess == compGuess) { //someone wins and we need to reset to a new game.
     guessesLeft = 9; //setting a intager data type.
     userGuesses = [];
     wins++; //addition function being triggered to add 1 win to win counter.
@@ -27,9 +26,8 @@ document.onkeyup = function (event) { //triggering the function.
     window.alert(" You Win! ");
     console.log("second if statement userGuess == compGuess ")
 
-
   }
-  else if (guessesLeft == 0) { //no more turns, lose logic
+  else if (guessesLeft == 0) { //no more turns, you lose logic
     guessesLeft = 9;
     userGuesses = [];
     losses++; //addition function being triggered to add 1 lose to losses counter.
@@ -38,8 +36,6 @@ document.onkeyup = function (event) { //triggering the function.
     console.log("You Lose!")
 
   }
-
-
   // 2) BUT!!! If it's wrong, then subtract from guess left and add to losses
   else if (compGuess != userGuess) { //wrong guess , but has more turns
     guessesLeft--;
@@ -48,20 +44,14 @@ document.onkeyup = function (event) { //triggering the function.
 
   console.log("user guesses so far is " + userGuesses);
 
-
   //connect variables to display
-document.getElementById("wins").innerHTML = ' Wins: ' + wins;
+  document.getElementById("wins").innerHTML = ' Wins: ' + wins;
 
-document.getElementById("losses").innerHTML = ' Losses: ' + losses;
+  document.getElementById("losses").innerHTML = ' Losses: ' + losses;
 
-document.getElementById("guesses-left").innerHTML = ' Guesses Left: ' + losses;
+  document.getElementById("guesses-left").innerHTML = ' Guesses Left: ' + guessesLeft;
 
-document.getElementById("your-guess").innerHTML = ' Your Guesses So Far: ' + userGuesses;
-
-
-
-
-
+  document.getElementById("your-guess").innerHTML = ' Your Guesses So Far: ' + userGuesses;
 
 
 
